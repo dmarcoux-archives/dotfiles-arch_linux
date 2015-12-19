94a77f5e8273e5ad360b31e427f1de3d3a81f43c
Dany Marcoux <danymarcoux@gmail.com>
2015-12-19 15:03:39 +0100

    generate_pkgs.sh: Made it generic

--- scripts/generate_pkgs.sh
+++ scripts/generate_pkgs.sh
@@ -1,4 +1,3 @@
 #/bin/bash
-# TODO: Add parameter to decide in which folder we are outputting the results
-pacman -Qqen &> ~/dotfiles/pkgs/personal/pkgs.txt
-aura -Qqem &> ~/dotfiles/pkgs/personal/aur-pkgs.txt
+pacman -Qqen &> ~/dotfiles/pkgs/pkgs.txt
+aura -Qqem &> ~/dotfiles/pkgs/aur-pkgs.txt
