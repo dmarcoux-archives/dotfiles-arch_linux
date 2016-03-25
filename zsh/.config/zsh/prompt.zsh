6f7fd5a6338024f863af9a7915b519cf1e0c8dc8
Dany Marcoux <danymarcoux@gmail.com>
2016-03-25 13:43:22 +0100

    zsh: Change git information in the prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -2,7 +2,8 @@
 autoload -Uz vcs_info
 
 # Only for Git
-zstyle ':vcs_info:*' formats " (%b) " enable git
+# with the format being: " branch_name "
+zstyle ':vcs_info:*' formats " %b " enable git
 
 # Call the package in a pre-command
 precmd() { vcs_info }
@@ -13,7 +14,8 @@ setopt prompt_subst
 #----- Prompt
 # %n -> username
 # %~ -> path
-# %f -> reset foreground color to the default
+# %F{green} -> set the foreground color to green
 # ${vcs_info_msg_0_}% -> git information, see above
+# %f -> reset foreground color to the default
 # Anything else is displayed as it is (For example, @)
-PROMPT='[%n@%~]${vcs_info_msg_0_}% $ %f'
+PROMPT='[%n@%~]%F{green}${vcs_info_msg_0_}% %f$ '
