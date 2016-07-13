3ca52c70d0ab99f3eac53e776dbe3494443fc309
Dany Marcoux <danymarcoux@gmail.com>
2016-07-13 11:54:51 +0200

    zsh: Start gpg-agent and use it for SSH keys

---
+++ zsh/.config/zsh/gpg-agent_ssh.zsh
@@ -0,0 +1,12 @@
+# From https://wiki.archlinux.org/index.php/GnuPG#SSH_agent
+
+# Start the gpg-agent if not already running
+if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
+  gpg-connect-agent /bye >/dev/null 2>&1
+fi
+
+# Set SSH to use gpg-agent
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+fi
