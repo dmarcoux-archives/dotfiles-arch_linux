e8e370a64a579a7b4d914b18c48382801e9f3e85
Dany Marcoux <danymarcoux@gmail.com>
2016-07-18 22:52:39 +0200

    zsh + pam_env: Set gpg-agent for SSH in pam_env
    It was in zsh before, but this approach with pam_env is shell agnostic

--- zsh/.config/zsh/gpg-agent_ssh.zsh
+++ zsh/.config/zsh/gpg-agent_ssh.zsh
@@ -1,12 +1,5 @@
+# Start gpg-agent if it is not already running
 # From https://wiki.archlinux.org/index.php/GnuPG#SSH_agent
-
-# Start the gpg-agent if not already running
 if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
   gpg-connect-agent /bye >/dev/null 2>&1
-fi
-
-# Set SSH to use gpg-agent
-unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-  export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
 fi
