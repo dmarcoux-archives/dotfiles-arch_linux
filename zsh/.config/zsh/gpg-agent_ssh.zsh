e4d25412333d1f539922f35849a7b1bca25bc7f5
Dany Marcoux <danymarcoux@gmail.com>
2016-07-18 23:45:35 +0200

    zsh: Set gpg-agent tty

--- zsh/.config/zsh/gpg-agent_ssh.zsh
+++ zsh/.config/zsh/gpg-agent_ssh.zsh
@@ -1,5 +1,11 @@
+# Set GPG TTY
+export GPG_TTY=$(tty)
+
 # Start gpg-agent if it is not already running
 # From https://wiki.archlinux.org/index.php/GnuPG#SSH_agent
 if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
   gpg-connect-agent /bye >/dev/null 2>&1
 fi
+
+# Refresh gpg-agent tty in case user switches into an X session
+gpg-connect-agent updatestartuptty /bye >/dev/null
