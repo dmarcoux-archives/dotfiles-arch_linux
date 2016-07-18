f45eef73d424d6ca30ad6ed59160fd08e00dfdb6
Dany Marcoux <danymarcoux@gmail.com>
2016-07-18 23:27:45 +0200

    zsh: Improve aliases readability

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,8 +1,8 @@
 #----- Default options
-alias ls='ls -a --color=auto' # Show all files and color the output
-alias mkdir='mkdir -pv' # No error when directory already exists, more feedback
+alias ls='ls --all --color=auto --format=horizontal --group-directories-first'
+alias mkdir='mkdir --parents --verbose' # No error when directory already exists, more feedback
 alias ping='ping -c 3' # Ping with 3 packets
-alias wget='wget -c' # Always resume download if partial file found
+alias wget='wget --continue' # Always resume download if partial file found
 alias history='history 1' # Retrieve all history (useful when using piping the output to grep/ag/whatever)
 
 #----- systemd
@@ -12,16 +12,16 @@ alias reboot='systemctl reboot'
 
 #----- Safety nets
 # Prompt when removing more than 3 files at a time and do not remove '/'
-alias rm='rm -I --preserve-root'
+alias rm='rm --interactive=once --preserve-root'
 # Fail to operate recursively on '/'
 alias chown='chown --preserve-root'
 alias chmod='chmod --preserve-root'
 alias chgrp='chgrp --preserve-root'
 
 #----- Confirmation
-alias mv='mv -i'
-alias cp='cp -i'
-alias ln='ln -i'
+alias mv='mv --interactive'
+alias cp='cp --interactive'
+alias ln='ln --interactive'
 
 #----- Package management and system upgrades
 alias pacman='sudo pacman'
@@ -40,7 +40,7 @@ alias bi='bundle install'
 alias be='noglob bundle exec' # Disable zsh glob operators with noglob (mostly for rake)
 alias bl='bundle list'
 alias -g rcop='rubocop' # Global alias
-alias rcophtml='rubocop -f html -o ./tmp/rubocop.html'
+alias rcophtml='rubocop --format html --out ./tmp/rubocop.html'
 
 #----- cURL
 alias curljson='curl --verbose --header "Content-Type: application/json" --data' # Then add '{"abc":"123","def":"'my text'"}' http://whatever.com
