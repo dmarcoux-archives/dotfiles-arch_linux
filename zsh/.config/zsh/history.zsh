3bddbc66837b52859aa2a93d234c6a4542294f13
Dany Marcoux <danymarcoux@gmail.com>
2016-07-21 14:11:35 +0200

    zsh: Add comments for history options

--- zsh/.config/zsh/history.zsh
+++ zsh/.config/zsh/history.zsh
@@ -1,6 +1,12 @@
 HISTFILE=~/.zhistory
 HISTSIZE=1000
 SAVEHIST=1000
+
+# Share history among all shells
 setopt SHARE_HISTORY
+
+# Do add command lines into the history if they are duplicates of the previous command line
 setopt HIST_IGNORE_DUPS
+
+# Remove superfluous blanks from each command line being added to the history
 setopt HIST_REDUCE_BLANKS
