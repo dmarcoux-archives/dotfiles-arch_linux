33f54b16ddf025f88790406035373d048d7db064
Dany Marcoux <danymarcoux@gmail.com>
2016-08-27 00:07:12 +0200

    zsh: Setup tag

---
+++ zsh/.config/zsh/tag.zsh
@@ -0,0 +1,6 @@
+# If tag is installed, set it up
+if (( $+commands[tag] )); then
+  tag() { command tag "$@"; source ${TAG_ALIAS_FILE:-/tmp/tag_aliases} 2>/dev/null }
+  alias ag=tag
+  export TAG_ALIAS_PREFIX=v
+fi
