9b363802e6f0e4c20772e37af93be302fc6eae0a
Dany Marcoux <danymarcoux@gmail.com>
2016-09-14 17:40:35 +0200

    zsh: Use noglob in the 'ag' alias

--- zsh/.config/zsh/tag.zsh
+++ zsh/.config/zsh/tag.zsh
@@ -1,6 +1,6 @@
 # If tag is installed, set it up
 if (( $+commands[tag] )); then
   tag() { command tag "$@"; source ${TAG_ALIAS_FILE:-/tmp/tag_aliases} 2>/dev/null }
-  alias ag=tag
+  alias ag='noglob tag'
   export TAG_ALIAS_PREFIX=v
 fi
