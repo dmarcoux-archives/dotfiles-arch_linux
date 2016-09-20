00a9eeec207adf0d9620aeb9d97dde73c386ba77
Dany Marcoux <danymarcoux@gmail.com>
2016-09-20 13:53:24 +0200

    git: Move aliases from git config to zsh
    Allows git related commands to be all under the same prefix (g), even
    the ones consisting of multiple commands piped together

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -46,6 +46,22 @@ alias vi='vim'
 
 #----- Git
 alias g='git'
+alias gbra='git branch'
+alias gchp='git cherry-pick'
+alias gcko='git checkout'
+alias gcom='git commit'
+alias gcoma='git commit --amend'
+alias gdi='git diff'
+alias gdis='git diff --staged'
+alias glo='git log --oneline'
+alias gpu='git push'
+alias gpuf='git push --force-with-lease'
+alias gpure='git pull --rebase --autostash'
+alias greb='git rebase'
+alias grebc='git rebase --continue'
+alias grebi='git rebase -i'
+alias gsh='git show'
+alias gsta='git status'
 
 #----- Clipboard copy/paste
 alias cbc='xsel --clipboard --input'
