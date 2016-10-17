e132de8f42df7aaaf65a74ce6fb8cb6ff4180b03
Dany Marcoux <danymarcoux@gmail.com>
2016-10-17 22:22:21 +0200

    zsh: Add git aliases using fzf to filter selection

--- zsh/.config/zsh/aliases/git.zsh
+++ zsh/.config/zsh/aliases/git.zsh
@@ -4,6 +4,8 @@ alias gbra='git branch'
 alias gchp='git cherry-pick'
 alias gcko='git checkout'
 alias gckob='git checkout -b'
+# Select a branch with fzf and switch to it
+alias gckof='git branch | fzf | cut --characters=3- | xargs --no-run-if-empty git checkout'
 alias gclo='git clone'
 alias gcom='git commit'
 alias gcoma='git commit --amend'
@@ -14,14 +16,20 @@ alias glo='git log --pretty=format:"%h %C(bold)|%C(reset) %s %C(bold)|%C(reset) 
 # Copy a string from the output of the glo alias (a commit hash for example)
 alias gloy='glo | yank'
 alias gmer='git merge'
+# Select a branch with fzf and merge it in the current branch
+alias gmerf='git branch | fzf | cut --characters=3- | xargs --no-run-if-empty git merge'
 alias gpu='git push'
 alias gpuf='git push --force-with-lease'
 alias gpure='git pull --rebase --autostash'
 alias greb='git rebase'
 alias grebc='git rebase --continue'
-alias grebi='git rebase --interactive'
+# From the output of the glo alias, select with fzf the commit hash to rebase on
+# TODO: Improve this to use xargs. It's not possible right now as xargs on Linux doesn't support -o, which prevent Vim from complaining about the input
+alias grebi='git rebase --interactive $(glo | fzf --ansi --no-sort | cut --delimiter=" " --fields=1)'
 alias gres='git reset'
 alias grev='git revert'
+# Select a commit with fzf and revert it
+alias grevf='glo | fzf --ansi --no-sort | cut --delimiter=" " --fields=1 | xargs --no-run-if-empty git revert'
 alias gsh='git show'
 alias gst='git status'
 alias gsta='git stash'
