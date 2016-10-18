14cd23a047f6dfe930af4d159ba8c725eb653a85
Dany Marcoux <danymarcoux@gmail.com>
2016-10-18 10:56:52 +0200

    fzf: Use the original ag command
    Since I'm using tag, which is aliased to ag, it sometimes behaves
    weirdly within fzf as the output isn't the same. With the original ag
    command, this doesn't happen

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -1,9 +1,9 @@
 # If fzf is installed, set it up
 if type fzf > /dev/null; then
   # Default command to use when input is tty
-  export FZF_DEFAULT_COMMAND='ag --hidden -g ""'
+  export FZF_DEFAULT_COMMAND='\ag --hidden -g ""'
   # Default command for the CTRL+T key binding
-  export FZF_CTRL_T_COMMAND='ag --hidden -g ""'
+  export FZF_CTRL_T_COMMAND='\ag --hidden -g ""'
 
   # Load default config files (completion and key bindings)
   # Completion at https://github.com/junegunn/fzf#fuzzy-completion-for-bash-and-zsh
