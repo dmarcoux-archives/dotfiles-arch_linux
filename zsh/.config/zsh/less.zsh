2e436158e58ea9719a414f02ea3e2b1fb872ebd5
Dany Marcoux <danymarcoux@gmail.com>
2016-12-31 10:51:57 -0500

    Revert "zsh: Configure command less"
    This reverts commit 9a7e8f0f85c6511cbce8a1eef1ce392697ced9a7. This
    somehow doesn't work when using within git, so I prefer to revert it

--- zsh/.config/zsh/less.zsh
+++
@@ -1,2 +0,0 @@
-# From https://www.topbug.net/blog/2016/09/27/make-gnu-less-more-powerful/
-export LESS='--quit-if-one-screen --ignore-case --status-column --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD --tabs=4 --window=-4'
