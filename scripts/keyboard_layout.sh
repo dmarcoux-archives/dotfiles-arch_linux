5ff22265b8df116b59fa6bddc1a88bacaa9112b8
Dany Marcoux <danymarcoux@gmail.com>
2017-02-08 13:03:29 +0100

    xmodmap: Add mapping for Eszett

--- scripts/keyboard_layout.sh
+++ scripts/keyboard_layout.sh
@@ -4,13 +4,9 @@
 setxkbmap -layout ca -variant multi
 
 # Customize the keyboard layout even more
-# Summary (details in .Xmodmap):
-# - Disable Left Control
-# - Caps Lock acts as Left Control
-# - AltGr + 4 produces €
-# - AltGr + Space produces a normal space
+# See details in xmodmap/.Xmodmap
 [[ -f ~/.Xmodmap ]] && xmodmap ~/.Xmodmap
 
-# Caps Lock acts as Left Control when pressed and held (set within .Xmodmap)
+# Caps Lock acts as Left Control when pressed and held (set with xmodmap)
 # Caps Lock acts as Escape when pressed and quickly released (set with xcape)
 xcape -e 'Control_L=Escape'
