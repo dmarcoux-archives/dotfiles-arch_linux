75424b22bedad821e9114de46330caff4999de69
Dany Marcoux <danymarcoux@gmail.com>
2017-03-23 22:28:50 +0100

    zsh: Add rop alias to remove orphan packages

--- zsh/.config/zsh/aliases/package_management.zsh
+++ zsh/.config/zsh/aliases/package_management.zsh
@@ -2,3 +2,6 @@ alias pacman='sudo pacman'
 
 # System update: Official packages, then AUR packages
 alias sup='pacman --sync --refresh --sysupgrade && pacaur --update --devel --needed'
+
+# Remove orphan packages
+alias rop='xargs --arg-file=<(sudo pacman --query --deps --unrequired --quiet) sudo pacman --remove --nosave --recursive'
