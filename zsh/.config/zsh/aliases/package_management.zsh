4ee78e2527956afb4a842d81e900ac42d2454705
Dany Marcoux <danymarcoux@gmail.com>
2017-04-06 11:41:47 +0200

    zsh: Use pacnanny in pacman alias
    The pacman alias is now a global alias, to avoid repetition in other
    aliases

--- zsh/.config/zsh/aliases/package_management.zsh
+++ zsh/.config/zsh/aliases/package_management.zsh
@@ -1,7 +1,7 @@
-alias pacman='sudo pacman'
+alias -g pacman='sudo pacnanny'
 
 # System update: Official packages, then AUR packages
 alias sup='pacman --sync --refresh --sysupgrade && pacaur --update --devel --needed'
 
 # Remove orphan packages
-alias rop='xargs --arg-file=<(sudo pacman --query --deps --unrequired --quiet) sudo pacman --remove --nosave --recursive'
+alias rop='xargs --arg-file=<(pacman --query --deps --unrequired --quiet) pacman --remove --nosave --recursive'
