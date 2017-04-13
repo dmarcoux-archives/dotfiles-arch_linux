4d929b62a9b6951547df4380b00e2124751fbee4
Dany Marcoux <danymarcoux@gmail.com>
2017-04-13 12:16:19 +0200

    zsh: Add services alias

--- zsh/.config/zsh/aliases/systemd.zsh
+++ zsh/.config/zsh/aliases/systemd.zsh
@@ -1,3 +1,6 @@
 alias sysd='systemctl'
 alias shutdown='systemctl poweroff'
 alias reboot='systemctl reboot'
+
+# Output all active and running systemd services (beside the core systemd services)
+alias services="systemctl list-units -t service --no-pager --no-legend | grep active | grep -v systemd | grep -v exited | awk '{ print \$1 }'"
