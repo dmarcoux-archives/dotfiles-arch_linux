77d0b4ba8ef34dc71ac6b3dd5d036a071112caab
Dany Marcoux <danymarcoux@gmail.com>
2017-04-25 17:07:12 +0200

    zsh: Add default options to ag in tag alias

--- zsh/.config/zsh/tag.zsh
+++ zsh/.config/zsh/tag.zsh
@@ -1,6 +1,12 @@
 # If tag is installed, set it up
 if (( $+commands[tag] )); then
   tag() { command tag "$@"; source ${TAG_ALIAS_FILE:-/tmp/tag_aliases} 2>/dev/null }
-  alias ag='noglob tag'
+
+  # ag doesn't have a way to set default options, so they are set in the alias below...
+  # color-match: bold blue foreground
+  # color-path: bold dark grey foreground
+  # color-line-number: bold dark grey foreground
+  alias ag='noglob tag --color-match="1;38;5;4" --color-path="1;38;5;10" --color-line-number="1;38;5;10"'
+
   export TAG_ALIAS_PREFIX=v
 fi
