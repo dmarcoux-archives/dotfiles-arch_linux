a7a9d99ebe8cd870f7a1421202cbd83707fcc61a
Dany Marcoux <danymarcoux@gmail.com>
2017-06-10 16:49:44 +0200

    zsh: Prevent nested ranger instances

---
+++ zsh/.config/zsh/ranger.zsh
@@ -0,0 +1,11 @@
+# If ranger is installed, set it up
+if type ranger > /dev/null; then
+  # https://wiki.archlinux.org/index.php/Ranger#Preventing_nested_ranger_instances
+  ranger() {
+      if [ -z "$RANGER_LEVEL" ]; then
+          /usr/bin/env ranger "$@"
+      else
+          exit
+      fi
+  }
+fi
