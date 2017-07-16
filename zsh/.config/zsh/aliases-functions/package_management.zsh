a527701a15c016d6ad4b38cfabb520b95bfc064a
Dany Marcoux <danymarcoux@gmail.com>
2017-07-16 17:06:37 +0200

    zsh: Improve package management functions

--- zsh/.config/zsh/aliases-functions/package_management.zsh
+++ zsh/.config/zsh/aliases-functions/package_management.zsh
@@ -1,7 +1,14 @@
-alias -g pacman='sudo pacnanny'
-
 # System update: Official packages, then AUR packages
-alias sup='pacman --sync --refresh --sysupgrade && pacaur --update --devel --needed'
+function sup() {
+  # TODO: Add another function which builds on top of pacfiles to deal with .pacnew/.pacsave files
+  sudo pacman --sync --refresh --sysupgrade && pacaur --update --devel --needed
+}
 
 # Remove orphan packages
-alias rop='xargs --arg-file=<(pacman --query --deps --unrequired --quiet) pacman --remove --nosave --recursive'
+# (with --arg-file, stdin remains unchanged when commands are run. Otherwise, stdin is redirected from /dev/null)
+alias rop='xargs --arg-file=<(pacman --query --deps --unrequired --quiet) --no-run-if-empty sudo pacman --remove --nosave --recursive'
+
+# List .pacnew, .pacsave files
+function pacfiles() {
+  find / -regextype posix-extended -regex ".+\.pac(new|save)" 2> /dev/null
+}
