a00f22b147f9d3e3552e5936c55920a7028deb47
Dany Marcoux <danymarcoux@gmail.com>
2017-11-24 14:21:31 +0100

    scripts: Replace generate_pkgs.sh

---
+++ scripts/backup_pkgs_and_systemd_units.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BACKUP_DIRS=(~/dotfiles/backup/$(hostname)/{pkgs,systemd_units})
+
+echo 'Create backup directories'
+mkdir --parents ${BACKUP_DIRS[@]}
+
+echo 'Generate list of official packages'
+pacman -Qqen &> ${BACKUP_DIRS[0]}/pkgs.txt
+
+echo 'Generate list of AUR packages'
+pacman -Qqem &> ${BACKUP_DIRS[0]}/aur-pkgs.txt
+
+echo 'Generate list of enabled systemd services'
+systemctl list-unit-files --type=service --state=enabled --no-legend --no-pager | tr --squeeze-repeats ' ' | cut --delimiter=' ' --fields=1 &> ${BACKUP_DIRS[1]}/enabled_services.txt
+
+echo 'Generate list of masked systemd units'
+systemctl list-unit-files --all --state=masked --no-legend --no-pager | tr --squeeze-repeats ' ' | cut --delimiter=' ' --fields=1 &> ${BACKUP_DIRS[1]}/masked_units.txt
--- scripts/generate_pkgs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir --parents pkgs/$(hostname)
-
-# Generate list of official packages
-pacman -Qqen &> ~/dotfiles/pkgs/$(hostname)/pkgs.txt
-
-# Generate list of AUR packages
-pacman -Qqem &> ~/dotfiles/pkgs/$(hostname)/aur-pkgs.txt
