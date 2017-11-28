865c0ad646e9b85a5a52578a9cfc27896096bc96
Dany Marcoux <danymarcoux@gmail.com>
2017-11-28 17:31:49 +0100

    scripts: Comment out masked systemd units list

--- scripts/backup_pkgs_and_systemd_units.sh
+++ scripts/backup_pkgs_and_systemd_units.sh
@@ -14,5 +14,6 @@ pacman -Qqem &> ${BACKUP_DIRS[0]}/aur-pkgs.txt
 echo 'Generate list of enabled systemd services'
 systemctl list-unit-files --type=service --state=enabled --no-legend --no-pager | tr --squeeze-repeats ' ' | cut --delimiter=' ' --fields=1 &> ${BACKUP_DIRS[1]}/enabled_services.txt
 
-echo 'Generate list of masked systemd units'
-systemctl list-unit-files --all --state=masked --no-legend --no-pager | tr --squeeze-repeats ' ' | cut --delimiter=' ' --fields=1 &> ${BACKUP_DIRS[1]}/masked_units.txt
+# Keep this as documentation
+# echo 'Generate list of masked systemd units'
+# systemctl list-unit-files --all --state=masked --no-legend --no-pager | tr --squeeze-repeats ' ' | cut --delimiter=' ' --fields=1 &> ${BACKUP_DIRS[1]}/masked_units.txt
