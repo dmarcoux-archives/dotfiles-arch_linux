3bb9b92c1d63c8a7855d1329b73313d027e6fd14
Dany Marcoux <danymarcoux@gmail.com>
2017-12-06 14:09:24 +0100

    zsh: Setup youtube-dl only if installed

--- zsh/.config/zsh/youtube-dl.zsh
+++ zsh/.config/zsh/youtube-dl.zsh
@@ -1,9 +1,12 @@
-# Download French and German videos from arte.tv
-#
-# $@: arte.tv URL(s) (either French or German)
-arte-dl() {
-  # Replace /fr/ or /de/ by {fr,de} in the arte.tv URL(s) (arte.tv handles correctly the switch of locale, even when done directly in their URLs)
-  # Pass the URL to xargs which expands {fr,de} (thanks to `sh -c`),
-  # then pass the output to youtube-dl (`-I %` positions the output after `youtube-dl`)
-  echo "$@" | sed -r "s@(/fr/|/de/)@/{fr,de}/@g" | xargs -I % sh -c "youtube-dl %"
-}
+# If youtube-dl is installed, set it up
+if type youtube-dl > /dev/null; then
+  # Download French and German videos from arte.tv
+  #
+  # $@: arte.tv URL(s) (either French or German)
+  arte-dl() {
+    # Replace /fr/ or /de/ by {fr,de} in the arte.tv URL(s) (arte.tv handles correctly the switch of locale, even when done directly in their URLs)
+    # Pass the URL to xargs which expands {fr,de} (thanks to `sh -c`),
+    # then pass the output to youtube-dl (`-I %` positions the output after `youtube-dl`)
+    echo "$@" | sed -r "s@(/fr/|/de/)@/{fr,de}/@g" | xargs -I % sh -c "youtube-dl %"
+  }
+fi
