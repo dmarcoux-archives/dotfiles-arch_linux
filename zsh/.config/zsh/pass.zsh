6228307a4b6a49a87fc96201309489749bafd432
Dany Marcoux <danymarcoux@gmail.com>
2017-12-06 16:31:47 +0100

    zsh: Add 2fa alias for pass

--- zsh/.config/zsh/pass.zsh
+++ zsh/.config/zsh/pass.zsh
@@ -2,4 +2,13 @@
 if type pass > /dev/null; then
   # Do not pollute the home directory
   export PASSWORD_STORE_DIR=$XDG_CONFIG_HOME/pass/passwords
+
+  # Copy two-factor authentication code to clipboard for a specific login
+  2fa(){
+    # List full paths of passwords stored by pass under $PASSWORD_STORE_DIR
+    # Keep the relative paths of these passwords without the .gpg extension (just like how pass does it)
+    # Select one login with fzf
+    # Retrieve the OTP code for the selected login and copy it to the clipboard
+    find $PASSWORD_STORE_DIR/otp -name "*.gpg" -print | sed -e "s|$PASSWORD_STORE_DIR/\(.*\)\.gpg|\1|g" | fzf --no-multi --reverse | xargs --no-run-if-empty pass otp --clip
+  }
 fi
