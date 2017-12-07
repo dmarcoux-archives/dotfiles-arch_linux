cd4a2165f375cfe7027fedbd664516b99dc9b986
Dany Marcoux <danymarcoux@gmail.com>
2017-12-07 16:19:55 +0100

    zsh: Add cbcf alias

--- zsh/.config/zsh/clipboard.zsh
+++ zsh/.config/zsh/clipboard.zsh
@@ -1,3 +1,6 @@
 # Useful when piped with other commands
 alias cbc='xsel --clipboard --input' # Copy to clipboard
 alias cbp='xsel --clipboard --output' # Paste from clipboard
+
+# With fzf, select a command from the command history and put it without its numeral part in the clipboard
+alias cbcf='fc -rl 1 | fzf --height=40% | sed "s/^ *\([0-9]*\) *//g" | cbc'
