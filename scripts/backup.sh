ed163ab8cb02627f29af25ee230ca125b0422ddb
Dany Marcoux <danymarcoux@gmail.com>
2017-12-18 23:20:28 +0100

    scripts: Change wording in backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,42 +3,42 @@
 # When changing this, do the same in restore_backup.sh
 BACKUP_DIRS=(~/dotfiles/backup/$(hostname)/{pkgs,systemd_units,keys})
 
-echo 'Create backup directories'
+echo 'Creating backup directories'
 mkdir --parents ${BACKUP_DIRS[*]}
 
-echo 'Generate list of official packages'
+echo 'Generating list of official packages'
 pacman -Qqen &> ${BACKUP_DIRS[0]}/pkgs.txt
 
-echo 'Generate list of AUR packages'
+echo 'Generating list of AUR packages'
 pacman -Qqem &> ${BACKUP_DIRS[0]}/aur-pkgs.txt
 
-echo 'Generate list of enabled systemd services'
+echo 'Generating list of enabled systemd services'
 systemctl list-unit-files --type=service --state=enabled --no-legend --no-pager | tr --squeeze-repeats ' ' | cut --delimiter=' ' --fields=1 &> ${BACKUP_DIRS[1]}/enabled_services.txt
 
 # Keep this as documentation
-# echo 'Generate list of masked systemd units'
+# echo 'Generating list of masked systemd units'
 # systemctl list-unit-files --all --state=masked --no-legend --no-pager | tr --squeeze-repeats ' ' | cut --delimiter=' ' --fields=1 &> ${BACKUP_DIRS[1]}/masked_units.txt
 
 echo 'Backup GPG and SSH keys?'
 select choice in "Yes" "No"; do
   case $choice in
-    Yes ) echo 'Export GPG keys and their trusts'
+    Yes ) echo 'Exporting GPG keys and their trusts'
           gpg --export-secret-keys --armor --output ${BACKUP_DIRS[2]}/secrets.asc
           gpg --export-ownertrust > ${BACKUP_DIRS[2]}/otrust.txt
 
-          echo 'Export SSH keys'
+          echo 'Exporting SSH keys'
           cp ~/.ssh/*id_rsa* ${BACKUP_DIRS[2]}
 
-          echo 'Put keys in a tar archive'
+          echo 'Archiving keys'
           $(cd ${BACKUP_DIRS[2]} && tar --create --file=keys.tar secrets.asc otrust.txt *id_rsa*)
 
-          echo 'Encrypt the tar archive'
+          echo 'Encrypting the tar archive'
           $(cd ${BACKUP_DIRS[2]} && gpg --symmetric --cipher-algo=AES256 keys.tar)
 
-          echo 'Remove exported GPG/SSH keys and the unencrypted tar archive'
+          echo 'Removing exported GPG/SSH keys and the unencrypted tar archive'
           $(cd ${BACKUP_DIRS[2]} && rm secrets.asc otrust.txt *id_rsa* keys.tar)
 
-          echo 'Reload GPG agent to clear the cached passphrase'
+          echo 'Reloading GPG agent to clear the cached passphrase'
           gpg-connect-agent reloadagent /bye 1> /dev/null
 
           break ;;
